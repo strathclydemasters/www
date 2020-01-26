c3761b2e63211bb28883a7b416117410784dc159
Richard Herbert <richard@infoweb.co.uk>
2020-01-26 12:22:11 +0000

    Correctly add support for YAML

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,11 @@
-module.exports = function(eleventyConfig) {
-	eleventyConfig.setTemplateFormats( "html,md" );
+// add yaml support with npm install js-yaml --save-dev
+const yaml = require( 'js-yaml' );
 
-	eleventyConfig.addPassthroughCopy("assets");
-	eleventyConfig.addPassthroughCopy("images");
+module.exports = function( eleventyConfig ) {
+	eleventyConfig.setTemplateFormats( 'html,md' );
 
+	eleventyConfig.addPassthroughCopy( 'assets' );
+	eleventyConfig.addPassthroughCopy( 'images' );
 
-	// add yaml support with npm install js-yaml --save-dev
-	const yaml = require( "js-yaml" );
-
-	module.exports = eleventyConfig => {
-		eleventyConfig.addDataExtension( "yaml", contents => yaml.safeLoad( contents ) );
-		eleventyConfig.addDataExtension( "yml", contents => yaml.safeLoad( contents ) );
-	};
-
+	eleventyConfig.addDataExtension( 'yml', contents => yaml.safeLoad( contents ) );
 };
