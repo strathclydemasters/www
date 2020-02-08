4810aff1d91b829d92399a2a78794517d60aceee
Richard Herbert <richard@infoweb.co.uk>
2020-02-08 09:09:25 +0000

    File site.yml changed to site.js and environment setting added

---
+++ _data/site.js
@@ -0,0 +1,9 @@
+module.exports = {
+	url: 'https://www.strathclydemasters.com/'
+	,title: 'Strathclyde Masters Basketball Tournament'
+	,environment: process.env.ELEVENTY_ENV
+	,metadata: {
+		description: 'The Strathclyde Masters is the premier basketball tournament in Scotland for men over 50, men over 60 and women over 45'
+		,keywords: 'strathclyde,masters basketball'
+	}
+};
