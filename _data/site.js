81ff9cf590ac09c7809987ac57e79b94b226f01c
Strathclyde Masters <strathclydemasterswebmaster@gmail.com>
2020-02-11 18:29:32 +0000

    Update site.js

--- _data/site.js
+++ _data/site.js
@@ -4,6 +4,6 @@ module.exports = {
 	,environment: process.env.ELEVENTY_ENV
 	,metadata: {
 		description: 'The Strathclyde Masters is the premier basketball tournament in Scotland for men over 50, men over 60 and women over 45'
-		,keywords: 'strathclyde,masters basketball'
+		,keywords: 'strathclyde,masters basketball,men,women'
 	}
 };
