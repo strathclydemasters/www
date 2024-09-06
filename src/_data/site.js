257c97caf1c67b9de22897fce5919fde63d00aed
Richard Herbert <richard@infoweb.co.uk>
2024-09-06 15:39:25 +0100

    Added 2025 tournament

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,8 +3,8 @@ module.exports = {
 	,title: 'Strathclyde Masters Basketball Tournament'
 	,environment: process.env.ELEVENTY_ENV
 	,metadata: {
-		description: 'The Strathclyde Masters is the premier basketball tournament in Scotland for men over 50, men over 60 and women over 45'
+		description: 'The Strathclyde Masters is the premier basketball tournament in Scotland for men over 40, 50, 60, 65 and women over 40 and 50'
 		,keywords: 'strathclyde,masters basketball,men,women'
 	}
-	,season: 2024
+	,season: 2025
 };
