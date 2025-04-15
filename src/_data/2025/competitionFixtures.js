f92dc27c9c1e3db8d801d646c6a29574f059a109
Richard Herbert <richard@infoweb.co.uk>
2025-04-15 20:29:03 +0100

    Added commented out competition fixtures

---
+++ src/_data/2025/competitionFixtures.js
@@ -0,0 +1,25 @@
+require( 'dotenv' ).config();
+
+const EleventyFetch = require( '@11ty/eleventy-fetch' );
+
+switch ( process.env.ELEVENTY_ENV ) {
+	case 'development':
+		url = 'http://admin.strathclydemasters.localhost/api/fixtures/773570B9-961D-45C7-A5A37F182A170103/competitions';
+	break;
+
+
+	default:
+		url = 'https://admin.strathclydemasters.com/api/fixtures/773570B9-961D-45C7-A5A37F182A170103/competitions';
+}
+
+module.exports = async function () {
+	return EleventyFetch( url, {
+		duration: '0d'
+		,type: 'json'
+		// ,fetchOptions: {
+		// 	headers: {
+		// 		'Authorization': process.env.API_TOKEN
+		// 	}
+		// }
+	} );
+};
